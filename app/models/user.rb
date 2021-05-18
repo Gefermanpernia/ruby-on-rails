d70b50fb5600eedeeead128450753f3aed7b9581
Geferman <gefermanp@gmail.com>
2021-05-17 22:28:34 -0400

    creacion de validadores en las pruebas e implementacion de los modelos para que pasen las pruebas (usando el modelo TDD)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	has_many :posts, class_name: "Post", foreign_key: "user_id"
+	validates :email, presence: true
+	validates :name, presence: true
+	validates :auth_token, presence: true
 end
